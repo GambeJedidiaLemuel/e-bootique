797505594a846e6e5e2edbc0c09277e1a0e7b268
unknown <gambejedidialemuel@gmail.com>
2026-02-08 15:43:58 +0100

    mon premier commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Nous contacter a travers:</h2>
+            <p>Telephone: 7484948749</p>
+            <p>Email: gambejedidialemuel@gmail.com</p>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+
+const Header = () => {
+    return <div>
+        <h1>VOICI NOTRE BOUTIQUE DE VENTE DES IPHONES</h1>
+    </div>
+}
+export default Header;
